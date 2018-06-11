9afd6da814a869196d8d3c8ac7f3b1733adca9ac
Mayeul Grivet-Roux <gmayeul@yahoo.fr>
2018-06-11 17:01:05 +0200

    Initialisation Maven

---
+++ src/test/java/fr/pizzeria/model/PizzaMemDaoTest.java
@@ -0,0 +1,44 @@
+package fr.pizzeria.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PizzaMemDaoTest {
+
+	@Test
+	public void testPizzaMemDao() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindAllPizzas() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSaveNewPizza() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testUpdatePizza() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDeletePizza() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFindPizzaByCode() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testPizzaExists() {
+		fail("Not yet implemented");
+	}
+
+}
